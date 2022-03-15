cdf8b3fe2b0b383d87b8613659c0e095c3fb74d1
Zappar <hello@zappar.com>
2022-03-15 18:25:11 +0000

    Minimal webpack project for Zappar AFrame instant-tracking

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import * as AFRAME from "aframe";
+import * as ZapparAFrame from "@zappar/zappar-aframe";
+
+console.log("Using AFRAME", AFRAME.version);
+console.log("Using ZapparAFrame", ZapparAFrame);
+
+window.addEventListener("load", setup);
+
+function setup() {
+
+    // When the user taps on the placement UI
+    const placementUI = document.getElementById("zappar-placement-ui");
+    placementUI.addEventListener("click", () => {
+
+        // Set placement-mode to false on the instant tracker group
+        const instantGroup = document.getElementById("instant-group");
+        instantGroup.setAttribute("zappar-instant", "placement-mode: false");
+
+        // Remove the placement UI
+        placementUI.remove();
+    });
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.ts',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js'
+	},
+	resolve: {
+		extensions: [".js", ".ts", ".wasm"]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: "head",
+			template: './src/index.html',
+			filename: 'index.html',
+			scriptLoading: "blocking"
+		})
+	],
+	devServer: {
+		static: './dist',
+		https: true,
+		host: '0.0.0.0'
+	},
+	module: {
+		rules: [
+			{ test: /\.ts?$/, loader: "ts-loader" },
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+          options: {
+            attributes: {
+              list: [
+                {
+                  tag: "img",
+                  attribute: "src",
+                  type: "src",
+                },
+                {
+                  tag: "a-asset-item",
+                  attribute: "src",
+                  type: "src",
+                },
+              ],
+            },
+          },
+        },
+      },
+			{
+				test: /zcv\.wasm$/,
+				type: "javascript/auto",
+				loader: "file-loader"
+			},
+		{
+			test: /\.(jpe?g|png|mp3|wav|zpt)$/,
+			use: [
+				{
+					loader: 'file-loader',
+					options: {
+						name: '[path][name].[ext]?[hash:7]',
+						context: 'src',
+					},
+				},
+			],
+		},
+		]
+	}
+};
